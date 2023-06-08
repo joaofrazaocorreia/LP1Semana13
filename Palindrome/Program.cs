b006e357183147872b9d0235ec2d380222de4d58
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-06-08 01:08:44 +0100

    Create method CheckPalindrome with function CheckLetters

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,7 +6,33 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+        private static bool CheckPalindrome(string s)
+        {
+            bool CheckLetters(string s)
+            {
+                int length = s.Length - 1;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (!(s[i] == s[length-i]))
+                        return false;
+                }
+                return true;
+            }
+
+            switch (s)
+            {
+                case null:
+                    throw new ArgumentNullException();
+
+                default:
+                    if (s.Length < 2)
+                        return true;
+
+                    else return CheckLetters(s);
+
+            }
         }
     }
 }
